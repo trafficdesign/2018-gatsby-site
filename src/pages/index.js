ecde9fe0c7538a57329e9a3f2cfbde621f9c8db1
Piotr F <piotr@piotrf.pl>
2018-10-09 21:21:38 +0200

    Attempt to fix og missing on root index redirect.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import locale from "browser-locale";
-import React, { PureComponent } from "react";
+import React, { Fragment, PureComponent } from "react";
+
+import { ogimage } from "assets/images";
+import Helmet from "react-helmet";
+import { META } from "config/meta";
 
 class RedirectIndex extends PureComponent {
   render() {
+    const lingo = "pl";
     const returnRedirect = () => {
       if (typeof window !== "undefined") {
         const getLocale = localStorage.getItem("lingo") || locale();
@@ -10,7 +15,26 @@ class RedirectIndex extends PureComponent {
         localStorage.setItem("lingo", setLocale);
         window.location.replace(`/${setLocale}/`);
       }
-      return <div />;
+      return (
+        <Fragment>
+          <Helmet encodeSpecialCharacters>
+            <html lang={lingo} />
+            <link rel="canonical" href="https://trafficdesign.pl" />
+            <meta name="description" content={META.description[lingo]} />
+            <meta name="keywords" content={META.keywords[lingo]} />
+            <meta name="language" content={lingo} />
+            <meta name="title" content={META.title[lingo]} />
+            <meta property="og:description" content={META.description[lingo]} />
+            <meta property="og:image:secure_url" content={ogimage} />
+            <meta property="og:image" content={ogimage} />
+            <meta property="og:locale" content={lingo} />
+            <meta property="og:title" content={META.title[lingo]} />
+            <meta property="og:url" content="https://trafficdesign.pl" />
+            <title>{META.title[lingo]}</title>
+          </Helmet>
+          <div />
+        </Fragment>
+      );
     };
     return returnRedirect();
   }
